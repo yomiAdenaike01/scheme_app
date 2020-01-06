ba93e2a851f6549d6d5361f46b6909868cb69529
Yomi Adenaike <adenaikeyomi@gmail.com>
2020-01-06 03:21:41 +0000

    added the experimental feature dialog for the messenger

--- src/views/Comms/Messenger.vue
+++ src/views/Comms/Messenger.vue
@@ -1,6 +1,17 @@
 <template>
   <el-row style="height:100%" type="flex" v-loading="loading">
     <el-col class="chatinfo_container">
+      <el-dialog class="experimental_feature_dialog" title="Experimental Feature" :visible.sync="experimentalFeatureDialogDisplay">
+        <div class="experimental_feature_dialog_title" slot="title">
+          <h2><i style="color:#E6A23C"  class="el-icon-warning"/> Experimental Feature Warning  </h2>
+          </div>
+        <p>
+          This is still an experimental feature and therefore, if you experience any bugs with this please take note of them and publish a bug report here.
+        </p>
+        <div class="button_center">
+        <el-button type="warning">Submit Bug Report</el-button>
+        </div>
+      </el-dialog>
       <StartNewChat />
       <PreviousChat />
     </el-col>
@@ -14,6 +25,12 @@ import MessengerBody from './components/MessengerBody'
 import { mapActions, mapState, mapMutations } from 'vuex'
 export default {
   name: 'Messenger',
+  data(){
+    return{
+      experimentalFeatureDialogDisplay:true
+    }
+  },
+  
   computed: {
     ...mapState('Admin', ['team']),
     ...mapState('Comms', ['transcripts']),
@@ -42,5 +59,18 @@ export default {
 .chat_sender_container {
   display: flex;
   padding: 1.5em;
+}
+.button_center{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  margin:2em;
+}
+.experimental_feature_dialog{
+  p{
+    text-align: center;
+    padding:20px;
+    font-size: 1.1em;
+  }
 }
 </style>
