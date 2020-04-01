c8f03b3ded9417bc6f3e80818d618330b144333c
Oluwayomi Adenaike <adenaikeyomi@gmail.com>
2020-04-01 14:14:20 +0100

    global search work

--- src/components/GlobalSearch.vue
+++ src/components/GlobalSearch.vue
@@ -6,17 +6,22 @@
         v-model="searchTerm"
         placeholder="Search what you want, we will take care of the rest..."
       ></el-input>
-      <div v-if="searchTerm > 0" class="search_results_container"></div>
-      <div class="predicitive_container">
-        <InformationDisplay
-          v-if="!hasEntries(cachedSearches)"
-          :display-text="{
-            heading: 'No previous searches',
-            content: 'All previous searches will appear here'
-          }"
-        />
-        <!-- cache searches and their results here -->
-        <div></div>
+
+      <div class="content_container">
+        <div v-if="searchTerm > 0" class="search_results_container">
+          <p>{{ searchTerm }}</p>
+        </div>
+        <div class="predicitive_container">
+          <InformationDisplay
+            v-if="!hasEntries(cachedSearches)"
+            :display-text="{
+              heading: 'No previous searches',
+              content: 'All previous searches will appear here'
+            }"
+          />
+          <!-- cache searches and their results here -->
+          <div></div>
+        </div>
       </div>
     </div>
   </el-dialog>
@@ -85,16 +90,27 @@ export default {
   methods: {
     completeSearch() {
       // if there is no previous results then append to cache
-    },
-    closeSearchBox() {
-      //   this.$emit("closeSearch");
-      console.log("closing");
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.global_search_container {
+  .el-input {
+    font-size: 1.3em;
+  }
+  &/deep/ .el-input__inner {
+    border: none;
+    font-size: 1.3em;
+  }
+}
+.content_container {
+  display: flex;
+  flex: 1;
+  border-top: 1px solid #efefef;
+  background: rgb(250, 250, 250);
+}
 .predicitive_container {
   display: flex;
   justify-content: center;
