0e768fbf1ac217d8fc12d643d61568a02a976738
Oluwayomi Adenaike <adenaikeyomi@gmail.com>
2020-05-03 18:20:47 +0100

    copy

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ import mutations from "./mutations";
 
 import Events from "./events";
 import Tasks from "./tasks";
-import Comms from "./comms/index";
+import Comms from "./comms";
 import Requests from "./requests";
 
 Vue.use(Vuex);
